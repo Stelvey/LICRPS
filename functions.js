652aa8d9b62e7bcee34e1308178bdabcc7e4efc0
Stelvey <stelveflame@gmail.com>
2022-09-20 00:11:51 +0300

    Implement the most basic RPS logic

--- functions.js
+++
@@ -1,5 +0,0 @@
-// Placeholder notifying user that the project is WIP
-export function warnWelcome() {
-    console.warn('This project is currently a WIP. Please, come back later. It will first be available as a CLI game and a final product will contain a fully-fledged GUI. Thank you for understanding!');
-}
-
--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-import { warnWelcome } from "./functions.js";
-
-// Noify user that the project is WIP
-warnWelcome();
-
-// GENERATE AND RETURN BOT'S CHOICE
+// Warn a user that the project is WIP
+console.warn(getWelcome());
 
 
-// PLAY A ROUND BY COMPARING USER'S AND BOT'S CHOICES AND RETURNING THE WINNER
 
+
+// FUNCTIONS PART
+// Placeholder notifying user that the project is WIP
+function getWelcome() {
+    return 'This project is currently a WIP. Please, come back later. It will first be available as a CLI game and a final product will contain a fully-fledged GUI. Thank you for understanding!';
+}
+
+// Generate and return AI's choice (012 as RPS)
+function getAiChoice() {
+    return Math.floor(Math.random() * 3);
+}
+
+// Evaluate the winner of a round (0 for P1, 1 for AI, 2 for Draw)
+function getRound(pOne, pTwo) {
+    if (pOne === pTwo) {
+        return 2;
+    } 
+    
+    switch(pOne + pTwo) {
+        case 1:
+            if (pOne === 1) {
+                return 0;
+            } else {
+                return 1;
+            }
+        case 2:
+            if (pOne === 0) {
+                return 0;
+            } else {
+                return 1;
+            }
+        case 3:
+            if (pOne === 2) {
+                return 0;
+            } else {
+                return 1;
+            }
+    }
+}
 
 // PLAY A GAME WITH SPECIFIC AMOUNT OF ROUNDS AND THEN REPORT A GAME WINNER
